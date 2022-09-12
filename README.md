9adbcd90ca9e02c2d577faf86a2db7e49ff45f55
unknown <daniel.spatacean94@gmail.com>
2022-09-12 13:47:02 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# axios-test
+
+## Purpose
+
+Reasearch, check and test if axios handles correctly proxy, using its native functionality, based on environment variables: `http_proxy`, `https_proxy`, `no_proxy`
+
+
+## Proxy server
+
+- Using a proxy server found on `https://www.freeproxylists.net/?s=u`
+- They are pretty unstable, so we might have to switch the proxy server that we are using for test
+
+
+## run
+1. npm install
+2. node app.js
+
+### Output
+
+Output will be written in standard output and also `app.log` (file logs contains a little bit more information)
+
+## Findings
+
+- Using some geolocation free APIs (`ip-api.com`, `ipapi.co`), looks like the requests are coming from the expected place (proxy/local)
+- Need a stable proxy server
+- From my past experience with axios and proxy, behind a corporate proxy, I had to explicitly set the proxy in the request options. This might be fixed now.
+- axios still have problems handling correctly `https` topic (`no_proxy` or `https` related proxies)
+- Using a proxy server over http for both, https_proxy and http_proxy + no_proxy containing values without ports, works fine
+- For working properly with `https_proxy`, in most cases, an `HttpsProxyAgent` is required
+
+
+## Related topics
+
+- https://github.com/axios/axios/issues/4369
+- https://github.com/axios/axios/pull/565
+- https://github.com/axios/axios/issues/3459
---
+++ app.js
@@ -0,0 +1,226 @@
+import fsPromises from 'fs/promises';
+import axios      from 'axios';
+import log4js     from 'log4js';
+
+log4js.configure({
+  appenders: {
+    consoleAppender: {type: 'console', category: 'app'},
+    fileAppender:    {type: 'file',    category: 'app', filename: 'app.log'},
+  },
+  categories: {
+    default: {appenders: ['consoleAppender', 'fileAppender'], level: 'debug'},
+    extra:   {appenders: ['fileAppender'],                    level: 'trace'},
+  },
+});
+
+const logger = log4js.getLogger('app');
+const extra  = log4js.getLogger('extra');
+
+// using a proxy server that I found on https://www.freeproxylists.net/?s=u and/or https://www.sslproxies.org/
+const proxyHost = '104.161.21.107';
+const proxyPort = '2019';
+
+// const proxyHost = '20.230.175.193'; // 130.41.55.190	8080
+// const proxyPort = '8080';
+
+const setProxy = () => {
+  logger.debug(`Setting proxy`);
+  process.env.http_proxy  = `http://${proxyHost}:${proxyPort}`;
+  process.env.https_proxy = `http://${proxyHost}:${proxyPort}`;
+  // https_proxy over http.
+  // if using https:
+  // https://github.com/axios/axios/issues/3459
+  // unless we are using some https agent, explicitly in the code
+};
+
+const setNoproxy = (noProxy) => {
+  logger.debug(`Setting no_proxy to ${noProxy}`);
+  process.env.no_proxy = noProxy;
+};
+
+const unsetNoproxy = () => {
+  if(process.env.no_proxy) {
+    logger.debug(`Unsetting no_proxy`);
+    delete process.env.no_proxy;
+  }
+};
+
+const unsetProxy = () => {
+  logger.debug(`Unsetting proxy`);
+  delete process.env.http_proxy;
+  delete process.env.https_proxy;
+  // If setting them to null/undefined, axios would try to make the request to 127.0.0.1:80
+  // process.env.http_proxy  = null; 
+  // process.env.https_proxy = null;
+};
+
+const configs = [
+  {
+    name:  'http://ip-api.com/json using proxy',
+    apiUrl: 'http://ip-api.com/json',
+    proxy: true,
+    expectation: 'proxy',
+  },
+  {
+    name: 'http://ip-api.com/json without proxy',
+    apiUrl: 'http://ip-api.com/json',
+    proxy: false,
+    expectation: 'direct',
+  },
+  {
+    name:  'http://ip-api.com/json using proxy and no_proxy = *',
+    apiUrl: 'http://ip-api.com/json',
+    proxy: true,
+    noProxy: '*',
+    expectation: 'direct',
+  },
+  {
+    name: 'https://ipapi.co/json using proxy',
+    apiUrl: 'https://ipapi.co/json',
+    proxy: true,
+    expectation: 'proxy',
+  },
+  {
+    name: 'https://ipapi.co/json without proxy',
+    apiUrl: 'https://ipapi.co/json',
+    proxy: false,
+    expectation: 'direct',
+  },
+  {
+    name: 'http://ipapi.co/json with proxy + no_proxy = ipapi.co',
+    apiUrl: 'http://ipapi.co/json',
+    proxy: true,
+    noProxy: 'ipapi.co',
+    expectation: 'direct',
+  },
+  {
+    name: 'http://ipapi.co/json with proxy + no_proxy = ipapi.co:443',
+    apiUrl: 'http://ipapi.co/json',
+    proxy: true,
+    noProxy: 'ipapi.co:443',
+    expectation: 'direct',
+  },
+  {
+    name: 'https://ipapi.co/json with proxy + no_proxy = ipapi.co',
+    apiUrl: 'https://ipapi.co/json',
+    proxy: true,
+    noProxy: 'ipapi.co',
+    expectation: 'direct',
+  },
+  {
+    name: 'https://ipapi.co/json with proxy + no_proxy = ipapi.co:443',
+    apiUrl: 'https://ipapi.co/json',
+    proxy: true,
+    noProxy: 'ipapi.co:443',
+    expectation: 'direct',
+  },
+];
+
+const axiosRequest = async(options) => {
+  let attempts   = 2;
+  let result     = {};
+
+  while(attempts > 0 && !result) {
+    try {
+      result = await axios(options);
+    } catch(err) {
+      if(err.response && err.response.status === 429) {
+        // try again
+        attempts -= 1;
+      } else {
+        attempts = 0;
+        throw err;
+      }
+    }
+  }
+
+  return result;
+};
+
+const clearLogFile = async() => {
+  try {
+    await fsPromises.unlink('./app.log')
+  } catch {
+    // ...
+  }
+};
+
+(async() => {
+  await clearLogFile();
+  logger.info(`
+  Started checking proxy handling with axios.
+  Configurations found ${configs.length}.
+  Proxy server that would be used: ${proxyHost}:${proxyPort}
+  `
+  );
+  logger.info('----------------------------------------------------------------------------------------------------------------')
+
+  const onlyConfigs  = configs.filter(item => item.only);
+  const finalConfigs = onlyConfigs.length ? onlyConfigs : configs;
+
+  for(const config of finalConfigs) {
+    const {name, proxy, expectation, noProxy, apiUrl} = config;
+
+    logger.info(`[${name}] Start`);
+
+    if(proxy) {
+      setProxy();
+    } else {
+      unsetProxy();
+    }
+
+    if(noProxy) {
+      setNoproxy(noProxy);
+    } else {
+      unsetNoproxy();
+    }
+
+    try {
+      logger.info(`[${name}] Sending request`);
+      const options = {
+        method:        'get',
+        responseType:  'json',
+        url:           apiUrl,
+        timeout:       40000, // proxy server might be slow, so take a reasonable timeout value
+        // proxy: false,
+      };
+      const response = await axiosRequest(options);
+
+      logger.info(`[${name}] Host on response.request is ${response.request.host}`);
+    
+      extra.trace(`Response \n`, response.data);
+  
+      if(response.data.query) {
+        // for ip-api.com responses
+        logger.info(`[${name}] Result: Request came from ${response.data.country}/${response.data.query}`);
+      }
+      if(response.data.country_name) {
+        // for ipapi.co responses
+        logger.info(`[${name}] Result: Request came from ${response.data.country_name}`);
+      }
+
+
+      // Check if expectation meets reality
+      const {ip, query} = response.data;
+      const source     = ip || query;
+
+      if(expectation === 'proxy') {
+        if(source === proxyHost) {
+          logger.info(`[${name}] Expectation met. Request came from Proxy Server: ${source}`);
+        } else {
+          logger.warn(`[${name}] Expectation not met. Request did not came from Proxy server: ${source}`);
+        }
+      } else if(expectation === 'direct') {
+        if(source !== proxyHost) {
+          logger.info(`[${name}] Expectation met. Request did not came from Proxy Server: ${source}`);
+        } else {
+          logger.warn(`[${name}] Expectation not met. Request came from Proxy server: ${source}`);
+        }
+      }
+    } catch(err) {
+      logger.warn(`[${name}] Failed`, err.message);
+    } finally {
+      logger.info('----------------------------------------------------------------------------------------------------------------');
+    }
+  }
+})();
